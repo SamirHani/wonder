efce964699d2949b47576a21ca6e1b6499e9a62f
samir <86558008+SamirHani@users.noreply.github.com>
2023-09-28 07:30:24 +0300

    added show class to the mega menu

--- main.js
+++ main.js
@@ -2,10 +2,7 @@ let megaMenu = document.querySelector(".mega-menu");
 let megaLink = document.querySelector(".mega");
 
 function opacity() {
- megaMenu.style.opacity = "1";
- megaMenu.style.zIndex = "100";
- megaMenu.style.top="calc(100% + 1px)";
- megaMenu.style.transition="top var(--transition-dur) , opacity var(--transition-dur)";
+megaMenu.classList.toggle("show");
 }
 console.log("hello world");
 
