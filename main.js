4725559901ef84a31738749c75d505f5aea93e7a
samir <86558008+SamirHani@users.noreply.github.com>
2023-09-28 07:10:45 +0300

    function onclick megaLink

---
+++ main.js
@@ -0,0 +1,8 @@
+let megaMenu = document.querySelector(".mega-menu");
+let megaLink = document.querySelector(".mege");
+
+megaLink.onclick = function () {
+ megaMenu.style.opacity = "1";
+ megaMenu.style.zIndex = "100";
+}
+
