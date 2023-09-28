c798694c64013e1236a9fb6009064a10d31d5a7e
samir <86558008+SamirHani@users.noreply.github.com>
2023-09-28 07:32:44 +0300

    deleted mega link

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 let megaMenu = document.querySelector(".mega-menu");
-let megaLink = document.querySelector(".mega");
 
 function opacity() {
 megaMenu.classList.toggle("show");
-  
-}
+};
+
 console.log("hello world");
 
