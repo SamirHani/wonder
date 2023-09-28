d83f40fbc8e9208fa3ea56892204daf6c87f9149
samir <86558008+SamirHani@users.noreply.github.com>
2023-09-28 07:18:51 +0300

    Update main.js

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ let megaLink = document.querySelector(".mega");
 function opacity() {
  megaMenu.style.opacity = "1";
  megaMenu.style.zIndex = "100";
+ megaMenu.style.top="calc(100% + 1px)";
+ megaMenu.style.transition="top var(--transition-dur) , opacity var(--transition-dur)";
 }
 console.log("hello world");
 
